66dd995fbbf09a253cf183a415fc0a61bba2e6f4
SalazarMalagon <andressalazar062858@gmail.com>
2025-09-23 17:39:30 -0500

    Add pendulum control examples and update tank logic
    Added new files for error-based and dynamics-based pendulum control examples, including Simulink models, fuzzy logic controllers, and animation S-functions. Updated TanqueAguaBooleano.m to adjust sensor thresholds and output logic, and modified TanqueAguaBooleanoSimulacion.slx accordingly.

--- Talleres/Taller 1/TanqueAguaBooleano.m
+++ Talleres/Taller 1/TanqueAguaBooleano.m
@@ -1,33 +1,33 @@
 function ft = TanqueAguaBooleano(h)
 %sensor B
-if h < 0.2
-    B=0;
+if h < 0.05
+    B = 0;
 else
-    B=1;
+    B = 1;
 end
 %sensor M
-if h < 0.5
-    M=0;
+if h < 0.6
+    M = 0;
 else
-    M=1;
+    M = 1;
 end
 %sensor C
-if h < 0.9
-    C=0;
+if h < 0.95
+    C = 0;
 else
-    C=1;
+    C = 1;
 end  
 %sensor A
 if h < 1
-    A=0;
+    A = 0;
 else
-    A=1;
+    A = 1;
 end
-Y1 = max(min((1-M),B), min((1-A), C));
-Y2 = min((1-C),B);
-Y3 = (1-B);
-%Y1 = 1-A;
-%Y2 = 1-C;
-%Y3 = 1-M;
+%Y1 = max(min((1-M),B), min((1-A), C));
+%Y2 = min((1-C),B);
+%Y3 = (1-B);
+Y1 = 1-A;
+Y2 = max((1-M), min((1-A),C));
+Y3 = 1-C;
 %Salida
-ft = 0.05*Y1 + 0.1*Y2 + 0.15*Y3;         
+ft = 0.02*Y1 + 0.04*Y2 + 0.06*Y3;
